388128cbe46cac8ed7187b129a7b70c35767975e
Pierluigi Coltré <pierluigi.coltre@gmail.com>
2025-07-18 22:38:50 +0200

    configuration for Jest and update  login tests

--- frontend/src/pages/Login.test.tsx
+++ frontend/src/pages/Login.test.tsx
@@ -1,10 +1,15 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Login from "./Login";
 import { BrowserRouter } from "react-router-dom";
 
 test("renders login form", () => {
   render(<BrowserRouter><Login /></BrowserRouter>);
-  expect(screen.getByText(/Login/i)).toBeInTheDocument();
+  // search title
+  expect(screen.getByRole('heading', { name: /login/i })).toBeInTheDocument();
+  // search username
   expect(screen.getByPlaceholderText(/Username/i)).toBeInTheDocument();
+  // search password
   expect(screen.getByPlaceholderText(/Password/i)).toBeInTheDocument();
+  // serch login button
+  expect(screen.getByRole('button', { name: /login/i })).toBeInTheDocument();
 });
