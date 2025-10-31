1bbc9daa5edf3f3afaf046b8d8e081f58813e352
Pierluigi Coltré <pierluigi.coltre@gmail.com>
2025-10-31 15:30:25 +0100

    removed unsupported parameters in cryptcotext

--- backend/auth.py
+++ backend/auth.py
@@ -19,13 +19,7 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 router = APIRouter()
-pwd_context = CryptContext(
-    schemes=["bcrypt"],
-    bcrypt__ident="2b",
-    bcrypt__variant="bcrypt",
-    bcrypt__default_backend="builtin",
-    deprecated="auto"
-)
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def truncate_password(password: str) -> str:
